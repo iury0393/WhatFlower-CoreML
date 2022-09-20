27a8195b59d7d0005aa8c9553d232be8c58eec8f
Iury Vasconcelos <iury0393@gmail.com>
2022-09-20 14:20:04 -0300

    Pequenas correções

--- WhatFlower/Controller/ViewController.swift
+++ WhatFlower/Controller/ViewController.swift
@@ -18,14 +18,16 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         imagePicker.delegate = self
         imagePicker.sourceType = .camera
-        imagePicker.allowsEditing = false
+        imagePicker.allowsEditing = true
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
-        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
             imageView.image = userPickedImage
         }
+        
+        imagePicker.dismiss(animated: true)
     }
 
 
